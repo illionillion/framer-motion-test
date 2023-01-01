d3e2339e09f530d1e9a54f8930ebefca46880c1f
yuto ichinose <yuto.0526.sano@gmail.com>
2023-01-01 22:47:42 +0900

    色々練習

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { useRef } from "react";
-import { motion } from "framer-motion";
+import { motion, useDragControls } from "framer-motion";
 
 function App() {
-  const constraintsRef = useRef(null)
+  const constraintsRef = useRef(null);
+  const controls = useDragControls();
+  const startDrag = (event: MouseEvent | TouchEvent | PointerEvent | React.MouseEvent<Element, MouseEvent> | React.TouchEvent<Element> | React.PointerEvent<Element>) => {
+    controls.start(event)
+    console.log(event);
+    
+  }
   return (
     <div className="App">
       <motion.div
@@ -28,9 +34,45 @@ function App() {
       >
         Hello, framer motion.
       </motion.div>
-      <motion.div ref={constraintsRef} style={{width: '100px', height:'100px', background: '#000'}}>
-        <motion.div drag dragConstraints={constraintsRef} style={{width: '20px', height:'20px', background: '#fff'}} />
+      <motion.div
+        ref={constraintsRef}
+        style={{ width: "100px", height: "100px", background: "#000" }}
+      >
+        <motion.div
+          drag
+          dragConstraints={constraintsRef}
+          style={{ width: "20px", height: "20px", background: "#fff" }}
+        />
+        <motion.div
+          drag
+          dragConstraints={constraintsRef}
+          style={{ width: "20px", height: "20px", background: "#fff" }}
+        />
       </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, y: [50, 0] }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
+        Hello, framer motion.
+      </motion.div>
+      <div onPointerDown={startDrag} />
+      <motion.div
+        drag="x"
+        dragDirectionLock
+        dragControls={controls}
+        style={{ width: "20px", height: "20px", background: "#000" }}
+        onDrag={(event, info)=>{
+          console.log(event);
+          console.log(info);
+          
+        }}
+        // dragListener={true}
+      />
+      <motion.div
+        drag="y"
+        style={{ width: "20px", height: "20px", background: "#000" }}
+      />
     </div>
   );
 }
